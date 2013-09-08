997e7fc361e7015a8ef57072471c89982664c375
Adnane Belmadiaf <adnane002@gmail.com>
2013-09-08 20:24:21 +0100

    Data fixes Added search fonction to the admin

--- www/radio/admin.py
+++ www/radio/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 
 from models import *
 
+class StationAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
 
 admin.site.register(Category)
 admin.site.register(Network)
 admin.site.register(Continent)
 admin.site.register(Country)
 admin.site.register(Language)
-admin.site.register(Station)
 admin.site.register(Source)
+admin.site.register(Station, StationAdmin)
+
